1dd12f2c86cb6ba6373f258341765a8d4f32f888
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-03-18 21:16:56 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+const taskForm = document.getElementById("create-task-form")
+let taskFormTextArea = document.querySelector('#new-task-description')
+const tasks = document.getElementById('tasks')
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  taskForm.addEventListener('submit', handleSubmit)
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    // grab text
+    let taskDescription = event.target.querySelector('#new-task-description').value
+    // reset text
+    tasks.innerHTML += `<li class="task">${taskDescription}</li>`
+    taskFormTextArea = ""
+  }
 });
+
